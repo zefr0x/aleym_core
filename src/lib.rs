a0d84d4b05dfd05963a51c07fd4c6403ffe88665
zefr0x <zer0-x.7ty50@aleeas.com>
2026-03-22 12:35:43 +0300

    docs(lib): mention the `full` feature

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 //!
 //! ## Feature flags
 //!
+//! * `full` -- build with all features enabled
 //! * `net_interface_clear` (default) -- build with support for clear interface to the internet
 //! * `net_interface_tor` -- build with support for Tor interface
 //! * `net_transport_tls` -- build with TLS encryption support (e.g. for HTTPS)
