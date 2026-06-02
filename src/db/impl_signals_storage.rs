2f9ef09678279f4b388973076ba7e4b5bf398677
zefr0x <zer0-x.7ty50@aleeas.com>
2026-06-02 22:11:10 +0300

    test(db/signals_storage): cover signals storage logic

--- src/db/impl_signals_storage.rs
+++ src/db/impl_signals_storage.rs
@@ -327,3 +327,370 @@ impl StorageConnection {
 		if count > 0 { Ok(sum / count) } else { Ok(Duration::ZERO) }
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use time::Duration;
+	use tracing_test::traced_test;
+
+	pub async fn setup_source(con: &StorageConnection) -> Uuid {
+		let root = con
+			.create_source_directory(None, "Root".to_owned(), None)
+			.await
+			.unwrap();
+
+		let source_id = con
+			.add_source(
+				root,
+				crate::inform::Parameters::TestPlaceholder,
+				crate::net::InterfaceType::TestPlaceholder,
+				"Test Source".to_owned(),
+				None,
+				true,
+			)
+			.await
+			.unwrap();
+
+		source_id
+	}
+
+	#[cfg(feature = "_informant")]
+	pub async fn setup_news_for_signals(con: &StorageConnection, source_id: Uuid) {
+		con.add_news(
+			source_id,
+			vec![super::super::impl_news_storage::InputNews {
+				source_provided_id: None,
+				uri: Some("https://example.com/1".to_owned()),
+				title: "News".to_owned(),
+				summary: None,
+				content: None,
+				published_at: None,
+				updated_at: None,
+			}],
+		)
+		.await
+		.unwrap();
+	}
+
+	#[cfg(feature = "_informant")]
+	pub async fn setup_signals_for_news(con: &StorageConnection, news_id: Uuid, now: OffsetDateTime) {
+		// Appearance signal
+
+		con.store_user_feedback_signal(UserFeedbackSignal::NewsApearanceSignal {
+			news: news_id,
+			happened_at: now,
+			duration: Duration::seconds(5),
+		})
+		.await
+		.unwrap();
+		con.store_user_feedback_signal(UserFeedbackSignal::NewsApearanceSignal {
+			news: news_id,
+			happened_at: now,
+			duration: Duration::seconds(50),
+		})
+		.await
+		.unwrap();
+		con.store_user_feedback_signal(UserFeedbackSignal::NewsApearanceSignal {
+			news: news_id,
+			happened_at: now,
+			duration: Duration::seconds(1000),
+		})
+		.await
+		.unwrap();
+
+		// Focus signal
+
+		con.store_user_feedback_signal(UserFeedbackSignal::NewsFocusSignal {
+			news: news_id,
+			done_at: OffsetDateTime::now_utc(),
+			duration: Duration::milliseconds(1500),
+		})
+		.await
+		.unwrap();
+
+		// Read signal
+
+		// Valid read signals
+		for percentage in [0, 50, 75, 100] {
+			con.store_user_feedback_signal(UserFeedbackSignal::NewsReadSignal {
+				news: news_id,
+				done_at: now + Duration::seconds(percentage as i64), // Just to keep some time difference
+				duration: Duration::seconds(30),
+				scroll_depth_percentage: percentage,
+			})
+			.await
+			.unwrap();
+		}
+
+		// Invalid read signals
+		con.store_user_feedback_signal(UserFeedbackSignal::NewsReadSignal {
+			news: news_id,
+			done_at: now,
+			duration: Duration::seconds(30),
+			scroll_depth_percentage: -1,
+		})
+		.await
+		.unwrap_err();
+		con.store_user_feedback_signal(UserFeedbackSignal::NewsReadSignal {
+			news: news_id,
+			done_at: now,
+			duration: Duration::seconds(30),
+			scroll_depth_percentage: 101,
+		})
+		.await
+		.unwrap_err();
+
+		// Vote signal
+
+		// Upvote
+		con.store_user_feedback_signal(UserFeedbackSignal::NewsExplicitVoteSignal {
+			news: news_id,
+			done_at: now,
+			is_up_vote: true,
+		})
+		.await
+		.unwrap();
+		// Downvote
+		con.store_user_feedback_signal(UserFeedbackSignal::NewsExplicitVoteSignal {
+			news: news_id,
+			done_at: now,
+			is_up_vote: false,
+		})
+		.await
+		.unwrap();
+	}
+
+	#[cfg(feature = "_informant")]
+	pub async fn setup_signals_for_source(con: &StorageConnection, source_id: Uuid, now: OffsetDateTime) {
+		con.store_source_feedback_signal(SourceFeedbackSignal::FetchSignal {
+			source: source_id,
+			done_at: now,
+			duration: Duration::seconds(5),
+			failure_code: None,
+			new_items_count: 10,
+			latest_publish_at: Some(now - Duration::hours(3)),
+			oldest_publish_at: Some(now - Duration::days(1)),
+		})
+		.await
+		.unwrap();
+		con.store_source_feedback_signal(SourceFeedbackSignal::FetchSignal {
+			source: source_id,
+			done_at: OffsetDateTime::now_utc(),
+			duration: Duration::seconds(5),
+			failure_code: Some(crate::inform::InformantErrorKind::Network),
+			new_items_count: 0,
+			latest_publish_at: None,
+			oldest_publish_at: None,
+		})
+		.await
+		.unwrap();
+		con.store_source_feedback_signal(SourceFeedbackSignal::FetchSignal {
+			source: source_id,
+			done_at: OffsetDateTime::now_utc(),
+			duration: Duration::seconds(1),
+			failure_code: Some(crate::inform::InformantErrorKind::Network),
+			new_items_count: 20,
+			latest_publish_at: None,
+			oldest_publish_at: None,
+		})
+		.await
+		.unwrap();
+		con.store_source_feedback_signal(SourceFeedbackSignal::FetchSignal {
+			source: source_id,
+			done_at: OffsetDateTime::now_utc(),
+			duration: Duration::seconds(1),
+			failure_code: None,
+			new_items_count: 30,
+			latest_publish_at: None,
+			oldest_publish_at: None,
+		})
+		.await
+		.unwrap();
+	}
+
+	#[cfg(feature = "_informant")]
+	#[tokio::test]
+	#[traced_test]
+	async fn user_feedback_signals_logic() {
+		let con = crate::db::impl_migration::tests::test_connection_and_migrations().await;
+		let source_id = setup_source(&con).await;
+		setup_news_for_signals(&con, source_id).await;
+
+		let now = OffsetDateTime::now_utc();
+
+		let news_list = con
+			.get_news_with_filter(
+				super::super::impl_news_storage::NewsFilter::default(),
+				super::super::SortOrder::Ascending,
+				super::super::impl_news_storage::TIME_MIN,
+				super::super::impl_news_storage::TIME_MAX,
+				100,
+			)
+			.await
+			.unwrap();
+		let news_id = news_list[0].id;
+
+		setup_signals_for_news(&con, news_id, now).await;
+
+		// Appearance signal
+
+		assert_eq!(
+			con.get_news_appearance_signals(news_id, now - Duration::hours(1), 100)
+				.await
+				.unwrap()
+				.num_items()
+				.await
+				.unwrap(),
+			3
+		);
+		assert_eq!(
+			con.get_source_appearance_signals(source_id, now - Duration::hours(1), 100)
+				.await
+				.unwrap()
+				.num_items()
+				.await
+				.unwrap(),
+			3
+		);
+
+		// Focus signal
+
+		assert_eq!(
+			con.get_focus_signals(source_id, now - Duration::hours(1), 100)
+				.await
+				.unwrap()
+				.num_items()
+				.await
+				.unwrap(),
+			1
+		);
+
+		// Read signal
+
+		assert_eq!(
+			con.get_read_signals(source_id, now - Duration::hours(1), 100)
+				.await
+				.unwrap()
+				.num_items()
+				.await
+				.unwrap(),
+			4
+		);
+
+		// Vote signal
+
+		assert_eq!(
+			con.get_vote_signals(source_id, now - Duration::hours(1), 100)
+				.await
+				.unwrap()
+				.num_items()
+				.await
+				.unwrap(),
+			2
+		);
+	}
+
+	#[cfg(feature = "_informant")]
+	#[tokio::test]
+	#[traced_test]
+	async fn source_feedback_signals_logic() {
+		let con = crate::db::impl_migration::tests::test_connection_and_migrations().await;
+		let source_id = setup_source(&con).await;
+
+		let now = OffsetDateTime::now_utc();
+
+		setup_signals_for_source(&con, source_id, now).await;
+
+		assert_eq!(
+			con.get_source_fetch_signals(Some(source_id), now - Duration::hours(1), 100)
+				.await
+				.unwrap()
+				.num_items()
+				.await
+				.unwrap(),
+			4
+		);
+		assert_eq!(
+			con.get_source_fetch_signals(None, now - Duration::hours(1), 100)
+				.await
+				.unwrap()
+				.num_items()
+				.await
+				.unwrap(),
+			4
+		);
+	}
+
+	#[cfg(feature = "_informant")]
+	#[tokio::test]
+	#[traced_test]
+	async fn average_new_items_per_fetch_logic() {
+		let con = crate::db::impl_migration::tests::test_connection_and_migrations().await;
+		let source_id = setup_source(&con).await;
+
+		let now = OffsetDateTime::now_utc();
+
+		let average = con.get_average_new_items_per_fetch(source_id, now, 100).await.unwrap();
+
+		assert_eq!(average, 0.0);
+
+		setup_signals_for_source(&con, source_id, now).await;
+
+		let average = con
+			.get_average_new_items_per_fetch(source_id, now - Duration::days(1), 100)
+			.await
+			.unwrap();
+
+		assert!(average > 0.0);
+		assert!(average <= 20.0);
+	}
+
+	#[cfg(feature = "_informant")]
+	#[tokio::test]
+	#[traced_test]
+	async fn maximum_new_items_observed_logic() {
+		let con = crate::db::impl_migration::tests::test_connection_and_migrations().await;
+		let source_id = setup_source(&con).await;
+
+		let now = OffsetDateTime::now_utc();
+
+		assert_eq!(con.get_maximum_new_items_observed(source_id, 100).await.unwrap(), 0);
+
+		setup_signals_for_source(&con, source_id, now).await;
+
+		assert_eq!(con.get_maximum_new_items_observed(source_id, 100).await.unwrap(), 30);
+	}
+
+	#[cfg(feature = "_informant")]
+	#[tokio::test]
+	#[traced_test]
+	async fn average_fetch_duration_logic() {
+		let con = crate::db::impl_migration::tests::test_connection_and_migrations().await;
+		let source_id = setup_source(&con).await;
+
+		let now = OffsetDateTime::now_utc();
+
+		assert_eq!(
+			con.get_average_fetch_duration(Some(source_id), OffsetDateTime::now_utc(), 100)
+				.await
+				.unwrap(),
+			Duration::ZERO
+		);
+
+		setup_signals_for_source(&con, source_id, now).await;
+
+		assert_eq!(
+			con.get_average_fetch_duration(Some(source_id), now - Duration::days(1), 100)
+				.await
+				.unwrap(),
+			Duration::seconds(3)
+		);
+		assert_eq!(
+			con.get_average_fetch_duration(None, now - Duration::days(1), 100)
+				.await
+				.unwrap(),
+			Duration::seconds(3)
+		);
+	}
+}
